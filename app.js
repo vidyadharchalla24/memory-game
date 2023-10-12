640d431f62522f3cac5cb513cbea0c3ab1b258dc
vidyadharchalla24 <vidyadharchalla333@gmail.com>
2023-10-12 14:58:29 +0530

    memory game outline created

---
+++ app.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    const cardArray = [
+        {
+            name : 'blank',
+            img : '/images/blank.png'
+        },
+        {
+            name : 'cheeseburger',
+            img : '/images/cheeseburger.png'
+        },
+        {
+            name : 'fries',
+            img : '/images/fries.png'
+        },
+        {
+            name : 'hotdog',
+            img : '/images/hotdog.png'
+        },
+        {
+            name : 'ice-cream',
+            img : '/images/ice-cream.png'
+        },
+        {
+            name : 'pizza',
+            img : '/images/pizza.png'
+        },
+        {
+            name : 'blank',
+            img : '/images/blank.png'
+        },
+        {
+            name : 'cheeseburger',
+            img : '/images/cheeseburger.png'
+        },
+        {
+            name : 'fries',
+            img : '/images/fries.png'
+        },
+        {
+            name : 'hotdog',
+            img : '/images/hotdog.png'
+        },
+        {
+            name : 'ice-cream',
+            img : '/images/ice-cream.png'
+        },
+        {
+            name : 'pizza',
+            img : '/images/pizza.png'
+        }
+    ]
+
+})
